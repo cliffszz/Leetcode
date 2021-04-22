690b825e8edb05244565f6cb13ae79d65d6884ec
cliffszz96 <cliffszz@126.com>
2021-04-22 13:36:43 +0800

    lab

--- Double Pointer/15_threeSum.cpp
+++ Double Pointer/15_threeSum.cpp
@@ -8,50 +8,25 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        // 找出a + b + c = 0
-        // a = nums[i], b = nums[left], c = nums[right]
         for (int i = 0; i < nums.size(); i++) {
-            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
-            if (nums[i] > 0) {
-                return result;
-            }
-            // 错误去重方法，将会漏掉-1,-1,2 这种情况
-            /*
-            if (nums[i] == nums[i + 1]) {
-                continue;
-            }
-            */
-            // 正确去重方法
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            if (nums[i] > 0) return res;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1;
             int right = nums.size() - 1;
-            while (right > left) {
-                // 去重复逻辑如果放在这里，0，0，0 的情况，可能直接导致 right<=left 了，从而漏掉了 0,0,0 这种三元组
-                /*
-                while (right > left && nums[right] == nums[right - 1]) right--;
-                while (right > left && nums[left] == nums[left + 1]) left++;
-                */
-                if (nums[i] + nums[left] + nums[right] > 0) {
-                    right--;
-                } else if (nums[i] + nums[left] + nums[right] < 0) {
-                    left++;
-                } else {
-                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
-                    // 去重逻辑应该放在找到一个三元组之后
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] > 0) right--;
+                else if (nums[i] + nums[left] + nums[right] < 0) left++;
+                else {
+                    res.push_back({nums[i], nums[left], nums[right]});
                     while (right > left && nums[right] == nums[right - 1]) right--;
-                    while (right > left && nums[left] == nums[left + 1]) left++;
-
-                    // 找到答案时，双指针同时收缩
+                    while (left < right && nums[left] == nums[left + 1]) left++;
                     right--;
                     left++;
                 }
             }
-
         }
-        return result;
+        return res;
     }
 };
