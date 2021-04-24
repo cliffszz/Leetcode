1bee038d29fabea22ba5b3dbac56950a3db6a660
cliffszz96 <cliffszz@126.com>
2021-04-24 20:31:34 +0800

    home

---
+++ Daliy Topic/377_combSums4.cpp
@@ -0,0 +1,70 @@
+//https://leetcode-cn.com/problems/combination-sum-iv/
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) {
+                if (n <= i && dp[i - n]  < INT_MAX - dp[i]) {
+                    dp[i] += dp[i - n];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        int ret = Solution().combinationSum4(nums, target);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
