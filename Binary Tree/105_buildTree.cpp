c1ee6d347ce05d6fc96176821739c0a4ac03a9e9
cliffszz96 <cliffszz@126.com>
2021-04-26 13:17:13 +0800

    lab

---
+++ Binary Tree/105_buildTree.cpp
@@ -0,0 +1,44 @@
+//https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* traversal(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0) return nullptr;
+        int rootVal = preorder[0];
+        TreeNode* root = new TreeNode(rootVal);
+        int delId;
+        for (int i = 0; i < inorder.size(); i++) {
+            if (inorder[i] == rootVal) {
+                delId = i;
+                break;
+            }
+        }
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + delId);
+        vector<int> rightInorder(inorder.begin() + delId + 1, inorder.end());
+        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + 1 + leftInorder.size());
+        vector<int> rightPreorder(preorder.begin() + 1 + leftInorder.size(), preorder.end());
+
+        root->left = traversal(leftPreorder, leftInorder);
+        root->right = traversal(rightPreorder, rightInorder);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0 || inorder.size() == 0) return nullptr;
+        return traversal(preorder, inorder);
+    }
+};
