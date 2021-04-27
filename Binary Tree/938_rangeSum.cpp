04493d133f75f1385f605e7102ebf27824d707c5
cliffszz96 <cliffszz@126.com>
2021-04-27 10:45:26 +0800

    lab

--- Binary Tree/938_rangeSum.cpp
+++ Binary Tree/938_rangeSum.cpp
@@ -40,3 +40,44 @@ public:
         return sum;
     }
 };
+
+class Solution2 {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        }
+        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+};
+
+class Solution3 {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        int sum = 0;
+        queue<TreeNode*> q({root});
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            if (node == nullptr) {
+                continue;
+            }
+            if (node->val > high) {
+                q.push(node->left);
+            } else if (node->val < low) {
+                q.push(node->right);
+            } else {
+                sum += node->val;
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return sum;
+    }
+};
