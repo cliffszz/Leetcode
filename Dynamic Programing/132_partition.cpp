3482b84efad444a15bc2cb9c465960ed40319a03
cliffszz96 <cliffszz@126.com>
2021-04-27 13:26:01 +0800

    lab

---
+++ Dynamic Programing/132_partition.cpp
@@ -0,0 +1,13 @@
+//https://leetcode-cn.com/problems/palindrome-partitioning-ii/
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCut(string s) {
+        
+    }
+};
