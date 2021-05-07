501ad190bab9563955075ed4d44584877c81c5a7
cliffszz96 <cliffszz@126.com>
2021-05-07 20:37:29 +0800

    lab

--- HOT 100/5_longestPalindrome.cpp
+++ HOT 100/5_longestPalindrome.cpp
@@ -6,23 +6,24 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string res;
+        if (s.size() < 2) return s;
+        string res = s.substr(0, 1);;
         int maxLen = 1;
         vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), 0));
         for (int i = 0; i < s.size(); i++) dp[i][i] = true;
         for (int len = 2; len <= s.size(); len++) {
-            for (int i = 0; i < s.size() - len; i++) {
+            for (int i = 0; i < s.size(); i++) {
                 int j = i + len - 1;
                 if (j >= s.size()) break;
                 if (s[i] != s[j]) dp[i][j] = false;
                 else {
-                    if (j - i > 3) dp[i][j] = dp[i + 1][j - 1];
+                    if (len > 2) dp[i][j] = dp[i + 1][j - 1];
                     else dp[i][j] = true;
                 }
                 if (dp[i][j] && len > maxLen) {
                     maxLen = len;
                     res = s.substr(i, len);
-                }
+                } 
             }
         }
         return res;
