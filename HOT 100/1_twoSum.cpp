57340e8f906ccd01eae79c0b1ac1caf85e6be481
cliffszz96 <cliffszz@126.com>
2021-05-08 10:20:09 +0800

    lab

---
+++ HOT 100/1_twoSum.cpp
@@ -0,0 +1,16 @@
+//https://leetcode-cn.com/problems/two-sum/
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(target - nums[i]) != map.end()) return {map[target - nums[i]], i};
+            else map[nums[i]] = i;
+        }
+        return {};
+    }
+};
