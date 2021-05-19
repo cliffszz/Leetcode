5d69bb4aa4588db3cc182b3f0fcba937aa4c2fdc
cliffszz96 <cliffszz@126.com>
2021-05-19 10:24:11 +0800

    dp review

--- Dynamic Programing/188_maxProfit4.cpp
+++ Dynamic Programing/188_maxProfit4.cpp
@@ -14,7 +14,7 @@ public:
         for (int j = 1; j < 2 * k; j += 2) {
             dp[0][j] = -prices[0];
         }
-        for (int i = 1;i < prices.size(); i++) {
+        for (int i = 1; i < prices.size(); i++) {
             for (int j = 0; j < 2 * k - 1; j += 2) { 
                 dp[i][j + 1] = max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
                 dp[i][j + 2] = max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
--- Dynamic Programing/309_maxProfit5.cpp
+++ Dynamic Programing/309_maxProfit5.cpp
@@ -23,6 +23,23 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) return 0;
+        vector<vector<int>> dp(prices.size(), vector<int>(2));
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[1][0] = max(dp[0][0], -prices[1]);
+        dp[1][1] = max(dp[1][1], dp[1][0] + prices[1]);
+        for (int i = 2; i < prices.size(); i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[prices.size() - 1][1];
+    }
+};
+
 void trimLeftTrailingSpaces(string &input) {
     input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
         return !isspace(ch);
