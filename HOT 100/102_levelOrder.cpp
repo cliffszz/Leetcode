b7f16aa4c5fb17703302837055482337ef5abd2f
cliffszz96 <cliffszz@126.com>
2021-05-27 13:13:06 +0800

    lab

---
+++ HOT 100/102_levelOrder.cpp
@@ -0,0 +1,36 @@
+//https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int n = que.size();
+            vector<int> level;
+            for (int i = 0; i < n; i++) {
+                TreeNode* cur = que.front();
+                que.pop();
+                level.push_back(cur->val);
+                if (cur->left) que.push(cur->left);
+                if (cur->right) que.push(cur->right);
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
+};
---
+++ HOT 100/105_buildTree.cpp
@@ -0,0 +1,66 @@
+//https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* backTrack(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0) return nullptr;
+        int rootVal = preorder[0];
+        TreeNode* root = new TreeNode(rootVal);
+        int id;
+        for (int i = 0; i < inorder.size(); i++) {
+            if (inorder[i] == rootVal) {
+                id = i;
+                break;
+            }
+        }
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + id);
+        vector<int> rightInorder(inorder.begin() + id + 1, inorder.end());
+        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + 1 + leftInorder.size());
+        vector<int> rightPreorder(preorder.begin() + 1 + leftInorder.size(), preorder.end());
+
+        root->left = backTrack(leftPreorder, leftInorder);
+        root->right = backTrack(rightPreorder, rightInorder);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return backTrack(preorder, inorder);
+    }
+};
+
+class Solution2 {
+public:
+    TreeNode* traversal(vector<int>& preorder, int leftPre, int rightPre, vector<int>& inorder, int leftIn, int rightIn) {
+        if (leftPre == rightPre) return nullptr;
+        int rootVal = preorder[leftPre];
+        TreeNode* root = new TreeNode(rootVal);
+        if (rightPre - leftPre == 1) return root;
+        int delId;
+        for (int i = leftIn; i <= rightIn; i++) {
+            if (inorder[i] == rootVal) {
+                delId = i;
+                break;
+            }
+        }
+
+        root->left = traversal(preorder, leftPre + 1, leftPre + 1 + delId - leftIn, inorder, leftIn, delId);
+        root->right = traversal(preorder, leftPre + 1 + delId - leftIn, rightPre, inorder, delId + 1, rightIn);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0 || inorder.size() == 0) return nullptr;
+        return traversal(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+    }
+};
