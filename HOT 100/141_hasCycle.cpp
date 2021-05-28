2972003791a98c4d6a4fc3690875f0939dd0a02e
cliffszz96 <cliffszz@126.com>
2021-05-28 16:24:52 +0800

    lab

---
+++ HOT 100/141_hasCycle.cpp
@@ -0,0 +1,22 @@
+//https://leetcode-cn.com/problems/linked-list-cycle/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
