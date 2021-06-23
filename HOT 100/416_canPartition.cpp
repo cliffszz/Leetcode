eb635227e9ca9bfcc0b26e9cbd8e2ecbac7a49db
cliffszz96 <cliffszz@126.com>
2021-06-23 16:57:48 +0800

    lab

--- HOT 100/416_canPartition.cpp
+++ HOT 100/416_canPartition.cpp
@@ -6,20 +6,19 @@ using namespace std;
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for (int& n : nums) {
-            sum += n;
+        int sum = 0, n = nums.size();
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+        sum = sum / 2;
+        vector<int> dp(sum + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= 0; j--) {
+                if (j - nums[i] >= 0) {
+                    dp[j] = dp[j] || dp[j - nums[i]];
+                }
+            }   
         }
-        if (sum % 2 == 1) {
-            return false;
-        }
-        int target = sum / 2;
-        vector<int> dp(target + 1, 0);
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = target; j >= nums[i]; j--) {
-                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
-            }
-        }
-        return dp[target] == target;
+        return dp[sum];
     }
 };
