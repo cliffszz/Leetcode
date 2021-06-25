b8553f72a26308b4dbb3cb91027dc458716b9348
cliffszz96 <cliffszz@126.com>
2021-06-25 10:29:54 +0800

    lab

--- HOT 100/461_hanmingDistance.cpp
+++ HOT 100/461_hanmingDistance.cpp
@@ -13,6 +13,10 @@ public:
     }
 };
 
+//我们可以使用Brian Kernighan算法进行优化，
+//具体地，该算法可以被描述为这样一个结论：记 f(x) 表示 x 和 x-1 进行与运算所得的结果
+//(即 f(x)=x&(x-1))，那么 f(x) 恰为 x 删去其二进制表示中最右侧的 1 的结果。
+
 class Solution2 {
 public:
     int hammingDistance(int x, int y) {
