0a45c4bce724fefa0505def3eea549ce076998ab
cliffszz96 <cliffszz@126.com>
2021-07-11 17:03:23 +0800

    go

---
+++ Offer/03_findRepeatNum.go
@@ -0,0 +1,28 @@
+package main
+
+func findRepearNumber(nums []int) int {
+	m := make(map[int]bool)
+	for _, n := range nums {
+		if _, exists := m[n]; exists {
+			return n
+		}
+		m[n] = true
+	}
+	return -1
+}
+
+func findRepearNumber2(nums []int) int {
+	i := 0
+	for i < len(nums) {
+		if nums[i] == i {
+			i++
+			continue
+		}
+		if nums[nums[i]] == nums[i] {
+			return nums[i]
+		} else {
+			nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+		}
+	}
+	return -1
+}
