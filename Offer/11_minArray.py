5b88635a5521438364b79d16f26236445d748701
cliffszz96 <cliffszz@126.com>
2021-07-18 22:54:25 +0800

    python study

---
+++ Offer/11_minArray.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            m = (i + j) // 2
+            if numbers[m] > numbers[j]: i = m + 1
+            elif numbers[m] < numbers[j]: j = m
+            else: return min(numbers[i:j])
+        return numbers[i]
---
+++ Offer/13_movingCount.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        def dfs(i, j, si, sj):
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: return 0
+            visited.add((i,j))
+            return 1 + dfs(i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj) + dfs(i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8)
+
+        visited = set()
+        return dfs(0, 0, 0, 0)
+
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        queue, visited = [(0, 0, 0, 0)], set()
+        while queue:
+            i, j, si, sj = queue.pop(0)
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: continue
+            visited.add((i,j))
+            queue.append((i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj))
+            queue.append((i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8))
+        return len(visited)
